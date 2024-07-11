7139e0da7a2573e37a33ac81108ec437d2099f03
cauarocha72 <150359289+cauarocha72@users.noreply.github.com>
2024-07-11 12:02:58 -0300

    Add files via upload

---
+++ Projeto2(Cauã Rocha)/Loja/javascript.js
@@ -0,0 +1,13 @@
+function Pesquisa(){
+var perguntas = document.getElementsByClassName('BarraDePesquisa').value
+var perguntas = Array['como faço pra comprar','quais tipos de produtos tem nessa loja']
+switch(perguntas){
+    case 0:
+        alert("Não é possivel fazer uma compre nesse momento")
+        break
+        case 1:
+            alert("Temos perifericos para Pc gamer")
+            break
+}
+}
+console.log(Pesquisa)
