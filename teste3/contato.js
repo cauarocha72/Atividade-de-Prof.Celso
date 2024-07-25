5bc049c376c67fcd5a6af72d5ac0d1cae427d478
cauarocha72 <150359289+cauarocha72@users.noreply.github.com>
2024-07-25 11:37:57 -0300

    Add files via upload

--- teste3/contato.js
+++ teste3/contato.js
@@ -1,13 +1,12 @@
 function formulario(){
-    var name = document.getElementById("name").value
-    var email = document.getElementById("email").value
-    var message = document.getElementById("message").value
+    var name = document.getElementById("name").value; // Obtém o valor do campo de nome
+    var email = document.getElementById("email").value; // Obtém o valor do campo de e-mail
+    var message = document.getElementById("message").value; // Obtém o valor do campo de mensagem
    
-    if(name,email,message){
-        alert("sucesso")
+    if(name && email && message){ // Verifica se todos os campos estão preenchidos
+        alert("Seu formulário foi enviado com sucesso, aguarde confirmação pelo e-mail."); // Exibe mensagem de sucesso
     }
     else{
-        alert("preencha o formulario")
+        alert("Por favor preencha o formulário."); // Exibe mensagem de erro se algum campo estiver vazio
     }
-
 }
